918133433086d9345d03143d85961085cfa16f33
Cristian Mircea Messel <mess110@gmail.com>
2015-09-19 15:32:42 +0300

    merge vendor files

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,43 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var order = require('gulp-order');
+var concat = require('gulp-concat');
 
 var paths = {
   coffee: ['src/**/*.coffee'],
   sass: ['src/**/*.sass'],
   html: ['src/**/*.html'],
+  vendor: {
+    js: [
+      './bower_components/angular/angular.js',
+      './bower_components/angular-animate/angular-animate.js',
+      './bower_components/angular-aria/angular-aria.js',
+      './bower_components/angular-material/angular-material.js'
+    ],
+    css: [
+      './bower_components/angular-material/angular-material.css'
+    ]
+  },
   output: './app/'
 };
 
 var coffee = require('gulp-coffee');
-gulp.task('coffee', function() {
+gulp.task('coffee', function () {
   gulp.src(paths.coffee)
     .pipe(coffee({bare: true})
     .on('error', gutil.log))
-    .pipe(gulp.dest(paths.output))
+    .pipe(gulp.dest(paths.output));
 });
+
+gulp.task('vendor', function () {
+  gulp.src(paths.vendor.js)
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest(paths.output));
+
+  gulp.src(paths.vendor.css)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest(paths.output));
+})
 
 var sass = require('gulp-sass');
 gulp.task('sass', function () {
@@ -36,4 +59,6 @@ gulp.task('watch', function() {
   gulp.watch(paths.html, ['html']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('dev', ['vendor', 'watch']);
+
+gulp.task('default', ['dev']);
