dc767136ccd7f867ce5d9768c4fb538ccd13970c
Cristian Mircea Messel <mess110@gmail.com>
2015-09-19 14:41:15 +0300

    add sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,30 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+
+var paths = {
+  coffee: ['src/**/*.coffee'],
+  sass: ['src/**/*.sass'],
+  output: './app/'
+};
+
 var coffee = require('gulp-coffee');
-
-gulp.task('default', ['coffee']);
-
 gulp.task('coffee', function() {
-  gulp.src('./src/**/*.coffee')
+  gulp.src(paths.coffee)
     .pipe(coffee({bare: true})
     .on('error', gutil.log))
-    .pipe(gulp.dest('./app/'))
+    .pipe(gulp.dest(paths.output))
 });
+
+var sass = require('gulp-sass');
+gulp.task('sass', function () {
+  gulp.src(paths.sass)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(paths.output));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.coffee, ['coffee']);
+  gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('default', ['watch']);
